7ce104d238aed2d117753c9aec00461751174091
Steven Hertz <hertzsteven@icloud.com>
2018-08-14 11:46:50 -0400

    added commit not yet in GitHub

--- learn source control/ViewController.swift
+++ learn source control/ViewController.swift
@@ -19,11 +19,9 @@ class ViewController: UIViewController {
         let r = "level 2"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        print("added commit not in github")
     }
-
 
 }
 
